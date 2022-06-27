23014f90f2de63bbc7beee42e274c4f583808ff3
Aditya Roshan Jha <64676132+AdityaRJha@users.noreply.github.com>
2022-06-28 04:29:47 +0530

    Main Activity implementation done.

--- app/src/main/java/com/example/wizard/uiux/MainActivity.kt
+++ app/src/main/java/com/example/wizard/uiux/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.wizard.uiux
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.wizard.R
+import com.example.wizard.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    var name = ""
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        binding.btnStartQuiz.setOnClickListener {
+            if (binding.tvName.text.toString().isEmpty()) {
+                binding.tvNameParent.error = "Please provide a name"
+            } else {
+                name = binding.tvName.text.toString()
+                startActivity(
+                    Intent(
+                        this,
+                        ChooseDifficulty::class.java,
+                    )
+                )
+            }
+        }
     }
 }
