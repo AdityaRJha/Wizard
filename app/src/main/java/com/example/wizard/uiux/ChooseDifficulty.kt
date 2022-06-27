2ed536402c5507dd25fa6a7a208f42947f1411d1
Aditya Roshan Jha <64676132+AdityaRJha@users.noreply.github.com>
2022-06-28 05:05:19 +0530

    Debugging, regarding the views.

--- app/src/main/java/com/example/wizard/uiux/ChooseDifficulty.kt
+++ app/src/main/java/com/example/wizard/uiux/ChooseDifficulty.kt
@@ -1,12 +1,40 @@
 package com.example.wizard.uiux
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.wizard.R
+import com.example.wizard.databinding.ActivityChooseDifficultyBinding
+
 
 class ChooseDifficulty : AppCompatActivity() {
+
+    private lateinit var binding: ActivityChooseDifficultyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_choose_difficulty)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_choose_difficulty)
+
+
+        binding.easyButton.setOnClickListener {
+            startNewActivity("easy")
+        }
+
+        binding.mediumButton.setOnClickListener {
+            startNewActivity("medium")
+        }
+
+        binding.hardButton.setOnClickListener {
+            startNewActivity("hard")
+        }
+    }
+
+    private fun startNewActivity(s: String) {
+        val bundle = Bundle()
+        bundle.putString("level", s)
+
+        val intent = Intent(this, QuestionActivity::class.java)
+        intent.putExtras(bundle)
+        startActivity(intent)
     }
 }
