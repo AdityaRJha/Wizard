a5473216fd3061622ce2047f2b8586948ba99077
Aditya Roshan Jha <64676132+AdityaRJha@users.noreply.github.com>
2022-06-28 03:58:00 +0530

    Layout of the Question activity is fixed.

---
+++ app/src/main/java/com/example/wizard/uiux/QuestionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wizard.uiux
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wizard.R
+
+class QuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question)
+    }
+}
