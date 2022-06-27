b5b8a92d395a4613df08fa9794b56607a8b40d48
Aditya Roshan Jha <64676132+AdityaRJha@users.noreply.github.com>
2022-06-28 02:49:00 +0530

    Added new activity to choose the difficulty of the questions.

---
+++ app/src/main/java/com/example/wizard/uiux/ChooseDifficulty.kt
@@ -0,0 +1,12 @@
+package com.example.wizard.uiux
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wizard.R
+
+class ChooseDifficulty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_difficulty)
+    }
+}
